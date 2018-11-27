049253f6caa33c035989d727386053d60958c9a0
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-27 15:48:08 -0500

    create, update, and delete in scratch

--- scratch/queries.js
+++ scratch/queries.js
@@ -6,15 +6,32 @@ const { MONGODB_URI } = require('../config');
 
 const Note = require('../models/note');
 
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+//   .then(() => {
+//     const searchTerm = 'lady gaga';
+//     let filter = {};
+
+//     if (searchTerm) {
+//       filter.title = { $regex: searchTerm, $options: 'i'};
+//     }
+//     return Note.find(filter).sort({updatedAt: 'desc'});
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect();
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    const searchTerm = 'lady gaga';
-    let filter = {};
-
-    if (searchTerm) {
-      filter.title = { $regex: searchTerm, $options: 'i'};
-    }
-    return Note.find(filter).sort({updatedAt: 'desc'});
+    const testId = '00000000000000000000000';
+    return Note.findById(testId);
   })
   .then(results => {
     console.log(results);
@@ -27,21 +44,63 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
     console.error(err);
   });
 
-  
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    const testId = '000000000000000000000000';
-    
-    return Note.findById(testId);
-  })
-  .then(results => {
-    console.log(results);
-  })
+    const newNote = {
+      title: 'test',
+      content: 'words'
+    };
+    Note.create({
+      title: newNote.title,
+      content: newNote.content
+    })
+      .then(results => {
+        console.log(results);
+      })
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+      });
+  });
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    return mongoose.disconnect();
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error(err);
+    const updateNote = {
+      _id: '000000000000000000000000',
+      title: 'foo',
+      content: 'bar'
+    };
+
+    Note.findByIdAndUpdate(updateNote._id, { $set: {title: updateNote.title, content: updateNote.content} })
+      .then(results => {
+        console.log(results);
+      })
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+      });
+  });
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    const deleteId = '000000000000000000000002';
+    
+    Note.findByIdAndRemove(deleteId)
+      .then(results => {
+        console.log(results);
+      })
+      .then(() => {
+        return mongoose.disconnect();
+      })
+      .catch(err => {
+        console.error(`ERROR: ${err.message}`);
+        console.error(err);
+      });
   });

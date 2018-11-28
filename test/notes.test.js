d448ded8351ef901f4fb42223af4ec64030eb9ee
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-28 14:45:26 -0500

    example post new item test working

--- test/notes.test.js
+++ test/notes.test.js
@@ -30,3 +30,33 @@ afterEach(function () {
 after(function () {
   return mongoose.disconnect();
 });
+
+describe('POST /api/notes', function () {
+  it('should create and return a new item when provided valid data', function () {
+    const newItem = {
+      'title': 'The best article about cats ever!',
+      'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor...'
+    };
+
+    let res;
+    return chai.request(app)
+      .post('/api/notes')
+      .send(newItem)
+      .then(function (_res) {
+        res = _res;
+        expect(res).to.have.status(201);
+        expect(res).to.have.header('location');
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.keys('id', 'title', 'content', 'createdAt', 'updatedAt')
+        return Note.findById(res.body.id);
+      })
+      .then(data => {
+        expect(res.body.id).to.equal(data.id);
+        expect(res.body.title).to.equal(data.title);
+        expect(res.body.content).to.equal(data.content);
+        expect(new Date(res.body.createdAt)).to.eql(data.createdAt);
+        expect(new Date(res.body.updatedAt)).to.eql(data.updatedAt);
+      });
+  });
+});

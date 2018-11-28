128fc598cbcdebe82c0ddbdd8b02e6e05e32a074
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-28 18:04:36 -0500

    fixed config to point to mlab db

--- config.js
+++ config.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://dev:devdev1@ds129966.mlab.com:29966/noteful',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
 };
-

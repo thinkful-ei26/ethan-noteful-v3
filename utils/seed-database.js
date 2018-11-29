f86a74a708b8dfdf68db81d16b767cc5a802ae33
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-29 14:59:16 -0500

    reseeded database with notes and folders

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -4,14 +4,23 @@ const mongoose = require('mongoose');
 
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
+const Folder = require('../models/folder');
 
-const { notes } = require('../db/seed/notes');
+const { notes, folders } = require('../db/seed/data');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => mongoose.connection.db.dropDatabase())
-  .then(() => Note.insertMany(notes))
+  .then(() => {
+    return Promise.all([
+      Note.insertMany(notes),
+      Folder.insertMany(folders),
+      Folder.createIndexes(),
+    ]);
+  })
   .then(results => {
-    console.info(`Inserted ${results.length} notes`);
+    // console.info(results);
+    console.info(`Inserted ${results[0].length} notes`);
+    console.info(`Inserted ${results[1].length} folders`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {

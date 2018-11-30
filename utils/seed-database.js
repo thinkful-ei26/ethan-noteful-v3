d0b270ceac9b682bfe9de4873b139dfc8adfe419
Ethan Frigon <ethan.frigon@gmail.com>
2018-11-30 11:51:52 -0500

    added tag schema and reseeded db

--- utils/seed-database.js
+++ utils/seed-database.js
@@ -5,8 +5,9 @@ const mongoose = require('mongoose');
 const { MONGODB_URI } = require('../config');
 const Note = require('../models/note');
 const Folder = require('../models/folder');
+const Tag = require('../models/tag');
 
-const { notes, folders } = require('../db/seed/data');
+const { notes, folders, tags } = require('../db/seed/data');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => mongoose.connection.db.dropDatabase())
@@ -15,12 +16,15 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
       Note.insertMany(notes),
       Folder.insertMany(folders),
       Folder.createIndexes(),
+      Tag.insertMany(tags),
+      Tag.createIndexes()
     ]);
   })
   .then(results => {
     // console.info(results);
     console.info(`Inserted ${results[0].length} notes`);
     console.info(`Inserted ${results[1].length} folders`);
+    console.info(`Inserted ${results[3].length} tags`);
   })
   .then(() => mongoose.disconnect())
   .catch(err => {
